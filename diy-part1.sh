f60a9729028b48769ba48c4b921165357644c205
qzyuebing <54806519+qzyuebing@users.noreply.github.com>
2022-05-27 14:41:46 +0800

    Add files via upload

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git Aibx https://github.com/Aibx/openwrt-packages' feeds.conf.default
+
